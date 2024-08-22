57c813196f556340a63c6f9b5a6d69c542196033
ReRecordable <vehicularmanslaughter0@gmail.com>
2024-08-22 18:23:48 -0400

    First config, originating from my Steam Deck

---
+++ Devices/valveSteamDeck/configuration.nix
@@ -0,0 +1,172 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ inputs, config, lib, pkgs, modules, ... }:
+
+{
+  imports =
+    [
+      # Import results from the hardware scan.
+      ./hardware-configuration.nix
+      # Import the jovian-nixos module
+      modules.jovian
+    ];
+
+  # Use the GRUB 2 EFI boot loader.
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    devices = [ "nodev" ];
+    gfxmodeEfi = "1280x800";
+    theme = inputs.nixos-grub-themes.packages.${pkgs.system}.nixos;
+  };
+  boot.loader.efi.efiSysMountPoint = "/efi";
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.plymouth.enable = true;
+  boot.plymouth.theme = "bgrt";
+  boot.initrd.verbose = false;
+  boot.consoleLogLevel = 0;
+  boot.kernelParams = [ "quiet" "udev.log_level=0" ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "nixos-steamdeck"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Enable non-free packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  #console = {
+  #  font = "Lat2-Terminus16";
+  #  keyMap = "us";
+  #  useXkbConfig = true; # use xkb.options in tty.
+  #};
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.excludePackages = [ pkgs.xterm ];
+  services.xserver.desktopManager.gnome.enable=true;
+  services.xserver.displayManager.startx.enable = true;
+  programs.xwayland.enable = true;
+
+  jovian.steam.enable = true;
+  jovian.steam.autoStart = true;
+  jovian.steam.desktopSession = "gnome-xorg";
+  jovian.devices.steamdeck.enable = true;
+  jovian.steam.user = "august";
+  programs.steam.extest.enable = true;
+  jovian.decky-loader = {
+    enable = true;
+    #user = "root";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  hardware.pulseaudio.enable = lib.mkForce false;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.august = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanger" ]; # Enable ‘sudo’ for the user.
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+    git
+    firefox
+    #vesktop
+    inputs.nix-software-center.packages.${system}.nix-software-center
+    catppuccin
+    catppuccin-gtk
+    catppuccin-cursors
+    numix-icon-theme-square
+    steamdeck-firmware
+    steam
+    extest
+    neofetch
+    prismlauncher
+    kdePackages.kate
+    gnomeExtensions.arcmenu
+    gnomeExtensions.dash-to-panel
+    gruvbox-dark-gtk
+    gruvbox-gtk-theme
+  ];
+
+  services.flatpak.enable = true;
+  programs.thunar.enable = true;
+  programs.thunar.plugins = with pkgs.xfce; [
+    thunar-archive-plugin
+    thunar-volman
+  ];
+  #xdg.portal.enable = true;
+  #xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
